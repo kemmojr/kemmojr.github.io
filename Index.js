b69e916b0933a9b336b0731cd161be164b35592d
KemmoJr <kemmojr@gmail.com>
2023-11-22 18:17:26 +1100

    better email sharing option

--- Index.js
+++ Index.js
@@ -1,2 +1,11 @@
+const email = 'kemmojr@gmail.com';
+
 function onLoad() {
+}
+
+function copyEmailToClipboard() {
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(email);
+
+    alert(`${email} copied to clipboard`);
 }
