7c81fe9f5c2e07470627ca5b8728981c149bf28e
KemmoJr <kemmojr@gmail.com>
2023-12-03 11:18:50 +1100

    Made navigation bar dynamic with active styling

--- Index.js
+++ Index.js
@@ -1,7 +1,50 @@
+//my current email
 const email = 'kemmojr@gmail.com';
 
-function onLoad() {
+function calculateOffset() {
+    // Calculate offset based on screen width or any dynamic content changes
+    const screenWidth = window.innerWidth;
+  
+    // Set different offsets based on screen width or other conditions as needed
+    let offset = 50; // Default offset
+  
+    if (screenWidth < 768) {
+      offset = 30; // Adjust offset for smaller screens
+    } else if (screenWidth >= 1200) {
+      offset = 70; // Adjust offset for larger screens
+    }
+  
+    return offset;
+  }
+
+// Function to determine which section is currently active based on scroll position
+function getActiveSection() {
+    // Get all the navigation links
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+  const scrollPosition = window.scrollY;
+
+  // Iterate through each section to find which one is in the viewport
+  document.querySelectorAll('section').forEach(section => {
+    const sectionTop = section.offsetTop - calculateOffset(); // Adjusted offset for better accuracy
+
+    if (scrollPosition >= sectionTop) {
+      const id = section.getAttribute('id');
+      // Remove active class from all links
+      navLinks.forEach(link => link.classList.remove('active'));
+      
+      // Add active class to the corresponding link
+      document.querySelector(`nav ul li a[href="#${id}"]`).classList.add('active');
+    }
+  });
 }
+
+// Listen for scroll events and call the function to update active section
+window.addEventListener('scroll', getActiveSection);
+
+// Initial call to set the active section on page load
+getActiveSection();
+
 
 function copyEmailToClipboard() {
     // Copy the text inside the text field
